c296bbb8a016c6f42241dbc48b3e8caadfc9c3b9
snowcovered2011 <17765293970@163.com>
2017-06-08 11:07:32 +0800

    Create dialog.vue

--- src/components/dialog/src/dialog.vue
+++ src/components/dialog/src/dialog.vue
@@ -1,105 +1,218 @@
 <template>
-    <div class="loading" v-show="visible">
-	    <div class="loading-mask">
-	    	<div class="loading-outter">
-		    	<div class="loading-wrap">
-		    		<div class="loading-ring"></div>
-		    	</div>
-	    		<div class="loading-rect"></div>
-	    		<div class="loading-text">{{ text }}</div>
-	    	</div>
-	    </div>
+    <div class="dialog-mask" v-show="visible">
+        <div class="dialog-wraper">
+            <div v-if="showInput" class="dialog-input-container">
+                <div class="dialog-input-header" v-if="showTitle">{{ title }}</div>
+                <div class="dialog-input-body">
+                    <input type="text" @input="changeBtnColor" :placeholder="inputPlaceholder" v-model="inputValue" class="dialog-input-textbox">
+                    <div v-show="showInputTestError" style="color: #d80000;margin-top: 10px;">{{this.inputTestError}}</div>
+                </div>
+                <div class="dialog-input-btn">
+                    <input class="dialog-input-btn-cancel f-c666666 dialog-b-right" type="button" @click="cancel" :value="cancelButtonText">
+                    <input :class="['dialog-input-btn-confirm', {'f-cdadada': !activeBtn, 'f-cf37937': activeBtn}]" type="button" @click="confirm" :value="confirmButtonText">
+                </div>
+            </div>
+            <div class="dialog-container" v-else>
+                <div class="dialog-header" v-if="showTitle">{{ title }}</div>
+                <div class="dialog-body f-c666666">
+                    <div>
+                        <div v-if="useText" v-html="text"></div>
+                        <div v-else>{{ message }}</div>
+                    </div>
+                </div>
+                <div class="dialog-btn" v-if="showOneBtn">
+                    <input type="button" class="dialog-one-btn f-cf37937" :value="oneBtnText" @click="oneBtn">
+                </div>
+                <div class="dialog-btn" v-else>
+                    <input class="dialog-btn-cancel f-c666666 dialog-b-right" type="button" @click="cancel" :value="cancelButtonText">
+                    <input class="dialog-btn-confirm f-cf37937" type="button" @click="confirm" :value="confirmButtonText">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
-export default {
-    props: {
+export default{
+	props: {
+        title: {
+          type: String,
+          default: '温馨提示'
+        },
+        message: {
+          type: String,
+          default: ''
+        },
+        confirmButtonText: {
+            type: String,
+            default: '确定'
+        },
+        cancelButtonText: {
+            type: String,
+            default: '取消'
+        },
         text: {
+            type: String,
+            default: ''
+        },
+        inputValue: {
             type: String,
             default: ''
         }
     },
-    data() {
-        return {
-        	visible: true
+	data() {
+  		return {
+  			visible: false,
+            showOneBtn: false,
+            showTitle: true,
+            showInput: false,
+            useText: false,
+            activeBtn: false,
+            showInputTestError: false
+  		}
+	},
+	methods: {
+        cancel(){
+            if(this.showInput){
+                this.activeBtn = false;
+            }
+            this.visible = false;
+            this.cancelButtonCallback();
+        },
+        confirm() {
+            if(this.showInput){
+                if(this.inputValue !== ''){
+                    if(!(this.inputTest.test(this.inputValue))){
+                        this.showInputTestError = true;
+                        return;
+                    }
+                    this.visible = false;
+                    this.activeBtn = false;
+                    this.confirmButtonCallback(this.inputValue);
+                }
+            }else{
+                this.visible = false;
+                this.confirmButtonCallback();
+            }
+        },
+        oneBtn() {
+            this.visible = false;
+            this.oneBtnCallback();
+        },
+        changeBtnColor(){
+            this.showInputTestError = false;
+            if(this.inputValue !== ''){
+                this.activeBtn = true;
+            }else{
+                this.activeBtn = false;
+            }
         }
-    },
-    created() {
-
-    }
+	},
 }
 </script>
 <style lang="scss" scoped>
-	.loading{
-		transition: opacity .3s linear;
-		width: 100%;
-		height: 100%;
-		position: fixed;
-		display: block;
-		text-align: center;
-		top: 0;
-		background-color: transparent;//背景透明
-		z-index: 9999;
-		&:after{
-			content: "";
-			display: inline-block;
-		}
-	}
-	.loading-mask{
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(0, 0, 0, .2);
-	}
-.loading-outter {
-    position: relative;
-    top: 50%;
-    margin-top: -0.35rem;
-    display: inline-block;
+	.dialog-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity .3s ease;
+    overflow: hidden;
+}
+
+.dialog-wraper {
     vertical-align: middle;
-    background-color: #222;
-    border-radius: .5rem;
-    height: .75rem;
-}
-.loading-wrap {
-    position: absolute;
-    width: .56rem;
-    height: .56rem;
-    top: .094rem;
-    left: .09rem
-}
-.loading-ring {
-    width: .56rem;
-    height: .56rem;
-    background-image: url("../../../assets/images/loading.svg");
-    background-size: cover;
-    position: absolute;
-}
-.loading-rect {
-    height: .18rem;
-    width: .18rem;
-    background-color: #FD404A;
-    border-radius: .05rem;
-    -webkit-transform: rotate3D(0, 0, 1, 45deg);
-    position: absolute;
-    left: .28rem;
-    top: .28rem;
+    display: table-cell;
 }
 
-.loading-text {
-    text-align: left;
-    color: #fff;
-    font-size: .24rem;
-    font-family: sans-serif;
-    line-height: .75rem;
-    padding-left: .85rem;
-    padding-right: .26rem;
+.dialog-container,
+.dialog-input-container{
+    margin: 0px .8rem;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all .3s ease;
+    font-family: Arial, "Microsoft YaHei";
 }
 
-.loading-enter,
-.loading-leave-active {
-    opacity: 0;
+.dialog-input-body{
+    width: 100%;
+    padding: .42rem .46rem .61rem .40rem;
+}
+
+.dialog-input-btn-cancel,
+.dialog-input-btn-confirm {
+    flex: 1;
+    height: .9rem;
+    line-height: .9rem;
+    font-size: .34rem;
+    border: none;
+    background: none;
+}
+
+.dialog-header,
+.dialog-input-header {
+    line-height: .56rem;
+    font-size: .36rem;
+    padding-top: .23rem;
+    text-align: center;
+    font-weight: bolder;
+    color: #1e1e1e;
+}
+
+.dialog-body {
+    padding: 0.12rem 0.8rem 0.3rem 0.8rem;
+    font-size: .30rem;
+    line-height: .4rem;
+}
+
+.dialog-btn,
+.dialog-input-btn {
+    border-top: 1px solid #e7e7e7;
+    display: flex;
+    width: 100%;
+}
+
+.dialog-one-btn{
+    height: .90rem;
+    line-height: .90rem;
+    font-size: .32rem;
+    border: none;
+    background: none;
+}
+
+.dialog-btn-cancel,
+.dialog-btn-confirm {
+    flex: 1;
+    height: .9rem;
+    line-height: .9rem;
+    font-size: .34rem;
+    border: none;
+    background: none;
+}
+
+.f-cf37937 {
+    color: #f37937
+}
+.f-cdadada{
+   color: #dadada;
+}
+
+.dialog-b-right {
+    border-right: 1px solid #e7e7e7;
+    border-radius: initial; //解决safari border继承bug
+}
+
+.dialog-input-textbox {
+    height: .88rem;
+    display: block;
+    border: 1px solid #e7e7e7;
+    font-size: .30rem;
+    text-indent: 1em;
 }
 </style>
