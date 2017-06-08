e1c7891265613ad63c677bd45adb41ef57d76340
snowcovered2011 <17765293970@163.com>
2017-06-08 10:38:10 +0800

    Update dialog.vue
    loading style change

--- src/components/dialog/src/dialog.vue
+++ src/components/dialog/src/dialog.vue
@@ -1,128 +1,105 @@
 <template>
-	<div class="dialog-container" v-if="visible">
-		<div class="mask"></div>
-		<div class="dialog-body">
-			<h2 class="dialog-title">{{ title }}</h2>
-			<div class="dialog-con" v-if="showText" v-html="text"></div>
-			<div class="dialog-con" v-else>
-				{{ message }}
-			</div>
-			<div class="dialog-btn">
-				<div class="dialog-onebtn"></div>
-				<div class="btn-container">
-					<div type="botton" name="cancel" value="取消" @click="cancel">{{ cancelButtonText }}</div>
-					<div type="botton" name="confirm" value="确定" @click="confirm">{{ confirmButtonText }}</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="loading" v-show="visible">
+	    <div class="loading-mask">
+	    	<div class="loading-outter">
+		    	<div class="loading-wrap">
+		    		<div class="loading-ring"></div>
+		    	</div>
+	    		<div class="loading-rect"></div>
+	    		<div class="loading-text">{{ text }}</div>
+	    	</div>
+	    </div>
+    </div>
 </template>
 <script>
-	export default {
-		props: {
-			title: {
-				type: String,
-				default: ''
-			},
-			showText: {
-				type: Boolean,
-				default: false
-			},
-			message: {
-				type: String,
-				default: ''
-			},
-			confirmButtonText: {
-				type: String,
-				default: '确定'
-			},
-			cancelButtonText: {
-				type: String,
-				default: '取消'
-			}
-		},
-		data(){
-			return{
-				visible: false
-			}
-		},
-		created(){
+export default {
+    props: {
+        text: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+        	visible: true
+        }
+    },
+    created() {
 
-		},
-		methods: {
-			cancel(){
-				this.visible = false;
-				this.cancelCallback();
-			},
-			confirm(){
-				this.visible = false;
-				this.confirmCallback();
-			}
-		}
-	}
+    }
+}
 </script>
 <style lang="scss" scoped>
-	.mask{
+	.loading{
+		transition: opacity .3s linear;
 		width: 100%;
 		height: 100%;
 		position: fixed;
+		display: block;
+		text-align: center;
+		top: 0;
+		background-color: transparent;//背景透明
+		z-index: 9999;
+		&:after{
+			content: "";
+			display: inline-block;
+		}
+	}
+	.loading-mask{
+		position: fixed;
 		top: 0;
 		left: 0;
-		// z-index: 999;
-		background-color: #000;
-		opacity: 0.9;
-	}
-	.dialog-container{
 		width: 100%;
 		height: 100%;
-		position: absolute;
-		top: 0;
-		left: 0;
-		overflow: hidden;
-		display: flex;
-		align-items: center;
+		background-color: rgba(0, 0, 0, .2);
 	}
-	.dialog-body{
-		position: absolute;
-		// top: 40%;
-		width: 70%;
-		left: 15%;
-		// margin: 0 1rem;
-		background-color: #fff;
-		color: #000;
-		border-radius: 10px;
-	}
-	.dialog-title{
-		text-align: center;
-		font-size: .36rem;
-		padding: .3rem 0 .1rem 0;
-		font-weight: 800;
-	}
-	.dialog-con{
-		font-size: .32rem;
-		padding: .3rem .4rem .4rem .4rem;
-		border-bottom: 1px solid #ccc;
-	}
-	.btn-container{
-		display: flex;
-		height: .7rem;
-		border-radius: 10px;
-	}
-	.btn-container div{
-		width: 50%;
-		text-align: center;
-		height: .7rem;
-		line-height: .7rem;
-		font-size: .3rem;
-		font-weight: 800;
-		border-radius: initial;//safri可能会继承border-radius，强行不让继承
-	}
-	.btn-container div:first-child{
-		border-right: 1px solid #ccc;
-		color: #ccc;
-	}
-	.btn-container div:last-child{
-		color: #FFA500;
-	}
+.loading-outter {
+    position: relative;
+    top: 50%;
+    margin-top: -0.35rem;
+    display: inline-block;
+    vertical-align: middle;
+    background-color: #222;
+    border-radius: .5rem;
+    height: .75rem;
+}
+.loading-wrap {
+    position: absolute;
+    width: .56rem;
+    height: .56rem;
+    top: .094rem;
+    left: .09rem
+}
+.loading-ring {
+    width: .56rem;
+    height: .56rem;
+    background-image: url("../../../assets/images/loading.svg");
+    background-size: cover;
+    position: absolute;
+}
+.loading-rect {
+    height: .18rem;
+    width: .18rem;
+    background-color: #FD404A;
+    border-radius: .05rem;
+    -webkit-transform: rotate3D(0, 0, 1, 45deg);
+    position: absolute;
+    left: .28rem;
+    top: .28rem;
+}
 
+.loading-text {
+    text-align: left;
+    color: #fff;
+    font-size: .24rem;
+    font-family: sans-serif;
+    line-height: .75rem;
+    padding-left: .85rem;
+    padding-right: .26rem;
+}
+
+.loading-enter,
+.loading-leave-active {
+    opacity: 0;
+}
 </style>
